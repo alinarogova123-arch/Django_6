074f8c0230efecdd2982a8bac04a9c59f59f5a6c
alinarogova123-arch <alinarogova123@gmail.com>
2026-03-20 17:43:28 +0300

    fix admin pages load time.

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from blog.models import Comment, Post, Tag
-from django.db.models import Count
+from django.db.models import Count, Prefetch
 
 
 def get_related_posts_count(tag):
@@ -20,7 +20,7 @@ def serialize_post(post):
         'image_url': post.image.url if post.image else None,
         'published_at': post.published_at,
         'slug': post.slug,
-        'tags': [serialize_tag(tag) for tag in post.tags.all().annotate(num_posts=Count('posts'))],
+        'tags': [serialize_tag(tag) for tag in post.tags.annotate(num_posts=Count('posts'))],
         'first_tag_title': post.tags.all()[0].title,
     }
 
@@ -34,7 +34,7 @@ def serialize_post_optimized(post):
         'image_url': post.image.url if post.image else None,
         'published_at': post.published_at,
         'slug': post.slug,
-        'tags': [serialize_tag(tag) for tag in post.tags.all().annotate(num_posts=Count('posts'))],
+        'tags': [serialize_tag(tag) for tag in post.tags.annotate(num_posts=Count('posts'))],
         'first_tag_title': post.tags.all()[0].title,
     } 
 
@@ -79,7 +79,7 @@ def post_detail(request, slug):
 
     likes = post.likes.all()
 
-    related_tags = post.tags.all().annotate(num_posts=Count('posts'))
+    related_tags = post.tags.annotate(num_posts=Count('posts'))
 
     serialized_post = {
         'title': post.title,
