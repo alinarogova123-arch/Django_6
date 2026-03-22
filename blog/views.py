569852e7d3a520bdfb92489aebe5f47bf3cee1b3
alinarogova123-arch <alinarogova123@gmail.com>
2026-03-22 19:13:33 +0300

    prefetch posts count for tags.

--- blog/views.py
+++ blog/views.py
@@ -4,6 +4,7 @@ from django.db.models import Count, Prefetch
 
 
 def serialize_post(post):
+    queryset = Tag.objects.annotate(num_posts=Count('posts'))
     return {
         'title': post.title,
         'teaser_text': post.text[:200],
@@ -12,7 +13,7 @@ def serialize_post(post):
         'image_url': post.image.url if post.image else None,
         'published_at': post.published_at,
         'slug': post.slug,
-        'tags': [serialize_tag(tag) for tag in post.tags.annotate(num_posts=Count('posts'))],
+        'tags': [serialize_tag(tag) for tag in post.tags.filter(posts=post).prefetch_related(Prefetch('posts', queryset=queryset))],
         'first_tag_title': post.tags.first().title,
     } 
 
