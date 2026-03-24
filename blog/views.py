1d710859592c6491eb9b4bde48757d8fc8f9ce35
alinarogova123-arch <alinarogova123@gmail.com>
2026-03-24 14:31:52 +0300

    Update views.py

--- blog/views.py
+++ blog/views.py
@@ -13,7 +13,7 @@ def serialize_post(post):
         'image_url': post.image.url if post.image else None,
         'published_at': post.published_at,
         'slug': post.slug,
-        'tags': [serialize_tag(tag) for tag in post.tags.filter(posts=post).prefetch_related(Prefetch('posts', queryset=queryset))],
+        'tags': [serialize_tag(tag) for tag in post.tags.prefetch_related(Prefetch('posts', queryset=queryset))],
         'first_tag_title': post.tags.first().title,
     } 
 
