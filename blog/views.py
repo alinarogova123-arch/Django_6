2cab6392c4c762cc7ea3aaf5e7015b4813f68b36
alinarogova123-arch <alinarogova123@gmail.com>
2026-03-24 15:42:25 +0300

    Added get_object_or_404

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from blog.models import Comment, Post, Tag
 from django.db.models import Count, Prefetch
 
@@ -13,7 +13,6 @@ def serialize_post(post):
         'image_url': post.image.url if post.image else None,
         'published_at': post.published_at,
         'slug': post.slug,
-
         'tags': [serialize_tag(tag) for tag in post.tags.annotate(num_posts=Count('posts'))],
         'first_tag_title': post.tags.first().title,
     } 
@@ -47,7 +46,7 @@ def index(request):
 
 
 def post_detail(request, slug):
-    post = Post.objects.get(slug=slug)
+    post = get_object_or_404(Post, slug=slug)
     comments = Comment.objects.filter(post=post).prefetch_related('author')
     serialized_comments = []
     for comment in comments:
@@ -88,7 +87,7 @@ def post_detail(request, slug):
 
 
 def tag_filter(request, tag_title):
-    tag = Tag.objects.get(title=tag_title)
+    tag = get_object_or_404(Tag, title=tag_title)
 
     most_popular_tags = Tag.objects.popular()[:5].annotate(num_posts=Count('posts'))
 
